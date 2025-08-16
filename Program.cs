5256b0e39969739863c46088dfcc9c9a69f091a4
CutoNaito <105581091+CutoNaito@users.noreply.github.com>
2025-08-16 23:34:07 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -8,13 +8,6 @@ var con = new Connection("Words");
 con.Test();
 con.CreateTable();
 con.InsertWords();
-// 
-// Player p1 = new Player("jarda");
-// Player p2 = new Player("honza");
-// 
-// TwoPlayerGame tp = new TwoPlayerGame(p1, p2);
-// 
-// tp.Start(new SqliteConnection(con.ConnectionString));
 
 MainLoop ml = new MainLoop(1200, 800, new SqliteConnection(con.ConnectionString));
 await ml.StartGame();
